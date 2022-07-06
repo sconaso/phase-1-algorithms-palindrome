12a1287fd0b19e598b0567c3d33daf9c900dfb99
Scott Nason <scottmnason@gmail.com>
2022-07-05 22:34:38 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let wordArray = word.split('')
+  let arrayLength = wordArray.length
+  let decrement = 1
+  let answer = true
+
+  // console.log('wordArray ', wordArray)
+  // console.log('arrayLength ', arrayLength)
+
+  for (let n = 0; n < arrayLength; n++ ) {
+    // console.log('decrement ', decrement)
+    // console.log(wordArray[n])
+    // console.log(wordArray[arrayLength - decrement])
+
+    if (wordArray[n] !== wordArray[arrayLength - decrement]){
+      answer = false;
+    }
+
+    // console.log('answer ', answer)
+    decrement++
+
+  }
+  return answer
 }
 
 /* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
+  PSUEDOCODE
+  Break apart the string into an array, with each element being an individual letter
+  Determine array length
+  Define a decrease counter, starting at 1
+  Set the default answer to true
+  Run a for loop, comparing the first element [0] with the last element of the array [n-decrease counter]
+  Increment the decrease counter by one
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +43,17 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("noon"))
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("noun"));
+
 }
 
 module.exports = isPalindrome;
